b962af53ef176c49552bfe250f63d19f97b28835
sinclair eric <sinclaireric60@gmail.com>
2023-12-26 12:43:20 +0100

    feat:logout + ui

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,29 +1,78 @@
 import * as React from 'react';
 
 import { useAuth } from '../../core';
-import {  ScrollView, Text, View } from 'react-native';
+import {  StyleSheet, Text, View,StatusBar,Pressable } from 'react-native';
 
 
 export const Profile = () => {
   const signOut = useAuth.use.signOut();
-    const iconColor ='#CCCCCC';
+
   return (
     <>
-
-      <ScrollView>
-        <View>
-          <Text>
-            'Mon compte'
-          </Text>
+<StatusBar />
+      
+        <View style={styles.container}>
          
+          
+          <View style={styles.img} />
+          <Text>
+            Maneng Sinclair
+          </Text>
 
-        {/*   <View className="my-8">
-            <ItemsContainer>
-              <Item text="settings.logout" onPress={signOut} />
-            </ItemsContainer>
-          </View> */}
+          <Text>
+            0 articles
+          </Text>
+
+          
+            <Pressable style={styles.logoutView} onPress={signOut}>
+              <Text style={styles.logoutText}> Deconnexion </Text>  
+            </Pressable>
+         
         </View>
-      </ScrollView>
+      
     </>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width:'100%',
+    alignItems:'center',
+    justifyContent: 'center',
+    padding: 40,
+  },
+  text: {
+    marginBottom:40,
+    borderColor:'#cccccc',
+    borderRadius:4,
+    width:'100%',
+  },
+  logoutView: {
+    marginTop:40,
+    paddingVertical:10,
+    alignItems:'center',
+    justifyContent:'center',
+    borderColor:'red',
+    borderWidth:1,
+    borderRadius:4,
+    width:'100%',
+  },
+  logoutText: {
+    
+    color:'red',
+    borderRadius:4,
+    
+  },
+
+  img: {
+    height:90,
+    width:90,
+    borderRadius:90,
+    backgroundColor:'#cccccc'
+  }
+
+
+ 
+});
