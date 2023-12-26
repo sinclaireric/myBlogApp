0595ebbc4be01291b4ac9e18f24b230d8e614e75
sinclair eric <sinclaireric60@gmail.com>
2023-12-26 16:34:31 +0100

    feat: add login feature

--- src/api/common/client.tsx
+++ src/api/common/client.tsx
@@ -1,8 +1,30 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-const BASE_URL = '';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+
+const httpLink = createHttpLink({
+  uri: 'https://api-dev.easycse.net/freya/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: '',
+      'X-DCE-PLATFORM':'SWIZY'
+    },
+  };
+});
+
+const link = authLink.concat(httpLink);
+
+
+
+const BASE_URL = 'https://api-dev.easycse.net/freya/graphql';
 
 const client = new ApolloClient({
-  uri: BASE_URL,
+  link,
   cache: new InMemoryCache(),
 });
 
